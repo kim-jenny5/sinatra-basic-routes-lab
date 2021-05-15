ec2e4685458e9d26a7e42ee99c7c871743fba8c5
Jenny Kim <kimj1905@gmail.com>
2021-05-14 22:02:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Jenny"
+    end
+
+    get '/hometown' do
+        "My hometown is Harrington Park, NJ"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'And July'"
+    end
 end
